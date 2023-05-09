5c1462cac5ced542655d6b5772c260899f7a1751
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-05-09 06:35:40 +0530

    ADD: Create New

---
+++ src/components/AddRecipe.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Header from "./Header";
+import Form from "./Form";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { useNavigate } from "react-router-dom";
+import "./AddRecipe.css";
+
+function AddRecipe() {
+  const defaultContent = {
+    entityMap: {},
+    blocks: [
+      {
+        key: "637gr",
+        text: "",
+        type: "unstyled",
+        depth: 0,
+        inlineStyleRanges: [],
+        entityRanges: [],
+      },
+    ],
+  };
+
+  let navigate = useNavigate();
+
+  const handleBackBtnClick = () => {
+    let path = `/`;
+    navigate(path);
+  };
+
+  return (
+    <div className="addRecipe">
+      <Header heading={"Let's Add a New Recipe"} />
+
+      <button className="btn addRecipe__backBtn" onClick={handleBackBtnClick}>
+        <HomeOutlinedIcon />
+        <br />
+        Go Back
+      </button>
+
+      <div className="addRecipe__form">
+        <Form
+          recipeTitle={""}
+          recipeCategory={"Appetizers & Snacks"}
+          recipeIngredients={[]}
+          description={defaultContent}
+          recipeImage={null}
+          type={"add"}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AddRecipe;
