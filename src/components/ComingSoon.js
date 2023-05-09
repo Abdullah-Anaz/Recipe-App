98c5fde4ffc4245cdc603b0a13c88e0c91088ebe
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-05-09 06:37:56 +0530

    ADD: Coming soon page

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import './ComingSoon.css'
+
+function ComingSoon() {
+  return (
+    <div className='comingsoon__page'>
+        <Header heading={"Coming Soon"}/>
+    </div>
+  )
+}
+
+export default ComingSoon
